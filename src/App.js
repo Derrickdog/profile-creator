c470f728277b7ff896d6f72d04fc95495e700c0b
Derrick <derrickryan10@gmail.com>
2022-07-05 10:58:24 -0400

    Remove ProfileDisplay.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import FormGeneral from './components/FormGeneral';
-// import ProfileDisplay from './components/ProfileDisplay';
 
 function App() {
   return (
     <div>
       <FormGeneral />
-      {/* <ProfileDisplay /> */}
     </div>
   );
 }
