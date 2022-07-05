66bb7813758987e1b5cd9073a30799620d30bbad
Derrick <derrickryan10@gmail.com>
2022-07-05 10:45:02 -0400

    Edit format

--- src/components/FormGeneral.js
+++ src/components/FormGeneral.js
@@ -6,15 +6,15 @@ class FormGeneral extends Component {
       super(props)
     
       this.state = {
-         firstName: '',
-         lastName: '',
-         email: '',
-         phoneNumber: '',
-         schoolName: '',
-         degree: '',
-         studies: '',
-         gpa: '',
-         graduationDate: ''
+         firstName: 'John',
+         lastName: 'Smith',
+         email: 'johnsmith@gmail.com',
+         phoneNumber: '123-456-7890',
+         schoolName: 'Binghamton University',
+         degree: 'BS',
+         studies: 'Computer Science',
+         gpa: '3.9',
+         graduationDate: 'May 2020'
       }
     }
 
@@ -78,6 +78,20 @@ class FormGeneral extends Component {
       })
     }
 
+    handleReset = () => {
+      this.setState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        phoneNumber: '',
+        schoolName: '',
+        degree: '',
+        studies: '',
+        gpa: '',
+        graduationDate: ''
+      })
+    }
+
   render() {
     const { firstName, lastName, email, phoneNumber, schoolName, degree, studies, gpa, graduationDate } = this.state;
     return (
@@ -85,50 +99,51 @@ class FormGeneral extends Component {
         <h2>Contact Information</h2>
         <form onSubmit={this.handleSubmit}>
           <div>
-              <label>First Name</label>
+              <label>First Name</label><br />
               <input type='text' value={firstName} onChange={this.handleFirstNameChange} />
           </div>
           <div>
-              <label>Last Name</label>
+              <label>Last Name</label><br />
               <input type='text' value={lastName} onChange={this.handleLastNameChange} />
           </div>
           <div>
-              <label>Email</label>
+              <label>Email</label><br />
               <input type='email' value={email} onChange={this.handleEmailChange} />
           </div>
           <div>
-              <label>Phone Number</label>
+              <label>Phone Number</label><br />
               <input type='phone' value={phoneNumber} onChange={this.handlePhoneNumberChange} />
           </div>
           <h2>Education Information</h2>
           <div>
-              <label>School Name</label>
+              <label>School Name</label><br />
               <input type='text' value={schoolName} onChange={this.handleSchool} />
           </div>
           <div>
-              <label>Degree</label>
+              <label>Degree</label><br />
               <input type='text' value={degree} onChange={this.handleDegree} />
           </div>
           <div>
-              <label>Studies</label>
+              <label>Studies</label><br />
               <input type='text' value={studies} onChange={this.handleStudies} />
           </div>
           <div>
-              <label>Graduation</label>
+              <label>Graduation</label><br />
               <input type='text' value={graduationDate} onChange={this.handleGraduation} />
           </div>
           <div>
-              <label>GPA</label>
+              <label>GPA</label><br />
               <input type='number' value={gpa} onChange={this.handleGpa} />
           </div>
+          <button type='reset' onClick={this.handleReset}>Reset</button>
         </form>
         <div>
           <h1>{firstName} {lastName}</h1>
           <p>{email}</p>
           <p>{phoneNumber}</p>
           <h2>{schoolName}</h2>
-          <p>{degree} {studies} {graduationDate}</p>
-          <p>{gpa}</p>
+          <p>{degree} {studies}</p>
+          <p>{graduationDate}</p>
         </div>
       </div>
     )
